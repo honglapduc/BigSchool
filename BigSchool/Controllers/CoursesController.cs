d01b58bb17bfd164eb50958582ae63aef531591e
HOME <HOME@MSI>
2023-03-08 15:49:33 +0700

    Add project files.

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Category.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ BigSchool/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+    }
+}   
